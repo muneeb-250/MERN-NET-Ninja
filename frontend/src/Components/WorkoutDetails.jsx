56c49a27db78fca9b63c8104030e1ef4a74ded97
Muneeb Ur Rehman <muneeburrehman250@gmail.com>
2023-04-10 09:02:13 +0500

    workout details card added (tailwindcss)

--- frontend/src/Components/WorkoutDetails.jsx
+++ frontend/src/Components/WorkoutDetails.jsx
@@ -3,11 +3,35 @@ import React from 'react'
 const WorkoutDetails = ({ workout }) => {
     return (
         <div className="workout-details">
-            <h4>{workout.title}</h4>
+            {/* <h4></h4>
             <p><strong>Load (Kg): </strong>{workout.load}</p>
             <p><strong>Reps: </strong>{workout.reps}</p>
-            <p>{workout.createdAt}</p>
+            <p>{workout.createdAt}</p> */}
+            <a
+                class="relative flex items-start justify-between rounded-xl border border-gray-100 bg-white my-3 p-4 shadow-xl sm:p-6 lg:p-8"
+                href="#"
+            >
+                <div class="pt-4 text-gray-500 ">
+
+                    <h3 class="mt-4 text-lg font-bold text-gray-900 sm:text-xl">
+                        {workout.title}
+                    </h3>
+
+                    <p class="mt-2 hidden text-sm sm:block">
+                        {workout.reps} reps <br />
+                        {workout.createdAt}
+                    </p>
+                </div>
+
+                <span
+                    class="rounded-full bg-green-100 px-3 py-1.5 text-xs font-medium text-green-600"
+                >
+                    {workout.reps}
+                </span>
+            </a>
+
         </div>
+
     )
 }
 
